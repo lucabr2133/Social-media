b41b55783aad29d3b4eb686f1064504e2a828903
lucabr2133 <lucab2188@gmail.com>
2025-12-01 15:51:19 -0300

    Changed the name of my website

--- front-odin-book/src/components/Header/Header.tsx
+++ front-odin-book/src/components/Header/Header.tsx
@@ -14,7 +14,7 @@ function MainHeader ({ userActive, setOpenDialog2s }:props) {
     <>
         <header   className='  bg-neutral-700/20 flex  fixed  '>
       <ul className='lg:w-[15%] flex flex-row lg:flex-col bottom-0 fixed justify-around lg:top-0 lg:justify-start bg-neutral-700/20 gap-5 *:min-h-[60px]  *:flex *:items-center *:text-3xl capitalize font-extrabold *:w-full w-full' style={{padding:'20px'}}>
-        <li className='hidden lg:block' ><h2 className='hidden lg:block'>My social media</h2></li>
+        <li className='hidden lg:block' ><h2 className='hidden lg:block'>Echo</h2></li>
         <li><Link to='/'><img width='20px' src='/homesvg.svg' alt='' /><h2 className='hidden lg:block'>Inicio</h2></Link></li>
         <li><Link to='/messages'><img width='20px' src='/messages.svg' /><h2 className='hidden lg:block'>Mensajes</h2></Link></li>
         {userActive && (<li><Link to={`/${userActive.username}`}><Useri></Useri> <h2 className='hidden lg:block'>Profile</h2></Link></li>)}
